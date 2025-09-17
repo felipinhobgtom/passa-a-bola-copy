f9ba26c8b31ce1e478c8d4db451aa3544198cee5
felipinho <felipinhobgouveia@gmail>
2025-09-17 17:08:30 -0300

    vao sem foderem

--- next_passa-a-bola/src/config/api.js
+++ next_passa-a-bola/src/config/api.js
@@ -7,6 +7,6 @@ const isServer = typeof window === 'undefined';
 
 const API_URL = isServer
 	? process.env.INTERNAL_API_URL || 'http://backend:8000'
-	: process.env.NEXT_PUBLIC_API_URL || '';
+	: '';
 
 export default API_URL;
